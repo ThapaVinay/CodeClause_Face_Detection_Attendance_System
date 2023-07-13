6d147ebb30011bd331c9f9aab4716375e1856945
lonewolf <thechampthapa@gmail.com>
2023-07-13 16:22:14 +0530

    added data to firebase database

---
+++ addDataToDatabase.py
@@ -0,0 +1,63 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("./serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL' : "https://facedetectionattendance-89f06-default-rtdb.firebaseio.com/"
+
+})
+
+
+ref = db.reference('Students')
+
+data = {
+    "666666" : {
+        "name" : "vinay thapa",
+        "major" : "btech cse",
+        "starting_year" : 2021,
+        "total_attendance" : 6,
+        "standing" : "G",
+        "year" : 3,
+        "last_attendence_time" : "2023-07-13 00:54:34"
+
+    },
+
+    "321654" : {
+        "name" : "murtaza",
+        "major" : "btech cse",
+        "starting_year" : 2021,
+        "total_attendance" : 6,
+        "standing" : "G",
+        "year" : 3,
+        "last_attendence_time" : "2023-07-13 00:54:34"
+
+    },
+
+    "852741" : {
+        "name" : "emily blunt",
+        "major" : "btech cse",
+        "starting_year" : 2021,
+        "total_attendance" : 6,
+        "standing" : "G",
+        "year" : 3,
+        "last_attendence_time" : "2023-07-13 00:54:34"
+
+    },
+
+    "963852" : {
+        "name" : "elon musk",
+        "major" : "btech cse",
+        "starting_year" : 2021,
+        "total_attendance" : 6,
+        "standing" : "G",
+        "year" : 3,
+        "last_attendence_time" : "2023-07-13 00:54:34"
+
+    }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
+
+
--- encodeGenerator.py
+++ encodeGenerator.py
@@ -2,6 +2,19 @@ import cv2
 import face_recognition
 import pickle
 import os 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+# connection with the firebase
+cred = credentials.Certificate("./serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL' : "https://facedetectionattendance-89f06-default-rtdb.firebaseio.com/",
+    'storageBucket' : "facedetectionattendance-89f06.appspot.com"
+
+})
+
 
 # importing the student images into a list 
 folderPath = './images'
@@ -11,6 +24,13 @@ studentIds = []
 for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
     studentIds.append(os.path.splitext(path)[0])   # splits the text from .png
+
+    # storing in the firebase
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
 
 
 def findEncodings(imagesList):
